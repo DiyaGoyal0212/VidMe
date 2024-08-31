e6053466e55988a91cd78c3391440826e33dc464
diya gupta <diyagupta.dg13@gmail.com>
2024-09-01 00:38:27 +0530

    Vidme clone by diya

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,29 +1,30 @@
 import express from "express";
 import { update, deletee, getUser, subscribe, unsubscribe, like, dislike } from "../controller/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
 // router.get("/test", test)
 
 //update a user
-router.put("/:id", update);
+router.put("/:id",verifyToken, update);
 
 //delete a user
-router.delete("/:id", deletee);
+router.delete("/:id",verifyToken, deletee);
 
 //get a user
-router.get("/find/:id", getUser);
+router.get("/find/:id",verifyToken, getUser);
 
 //subscribe a user
-router.put("/sub/:id", subscribe);
+router.put("/sub/:id",verifyToken, subscribe);
 
 //unsubscribe a user
-router.put("/unsub/:id", unsubscribe);
+router.put("/unsub/:id",verifyToken, unsubscribe);
 
 //like a video
-router.put("/like/:videoId", like);
+router.put("/like/:videoId",verifyToken, like);
 
 //dislike a video
-router.put("/dislike/:videoId", dislike);
+router.put("/dislike/:videoId",verifyToken, dislike);
 
 export default router;
--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,8 +1,20 @@
 import express from "express";
-import {  } from "../controller/video.js";
+import { trend } from "../controller/video.js";
+import {verifyToken} from "../verifyToken.js"; 
 
 const router = express.Router();
 
 // router.get("/test", test)
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, addVideo);
+router.delete("/:id", verifyToken, addVideo);
+router.get("/find/:id",addVideo);
+
+router.put("/view/:id",addView);
+router.get("/trend", trend);
+router.get("/random", random);
+router.get("/sub", verifyToken, sub);
+router.get("/tags", getByTag);
+router.get("/search", search);
 
 export default router;
